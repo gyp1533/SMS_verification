4ea962eb8b4541c1049563270debe266d93f5ac7
gyp1533 <gyp1533@163.com>
2019-03-09 16:32:23 +0800

    首次提交

---
+++ src/com/miaodiyun/httpApiDemo/common/Config.java
@@ -0,0 +1,26 @@
+package com.miaodiyun.httpApiDemo.common;
+
+/**
+ * 系统常量
+ */
+public class Config {
+	/**
+	 * url前半部分
+	 */
+	public static final String BASE_URL = "https://api.miaodiyun.com/20150822";
+
+	/**
+	 * 开发者注册后系统自动生成的账号，可在官网登录后查看
+	 */
+	public static final String ACCOUNT_SID = "ade939b3672046f49b2399e9d1722431";
+
+	/**
+	 * 开发者注册后系统自动生成的TOKEN，可在官网登录后查看
+	 */
+	public static final String AUTH_TOKEN = "d27dc817c6d549d28a7747de29615468";
+
+	/**
+	 * 响应数据类型, JSON或XML
+	 */
+	public static final String RESP_DATA_TYPE = "json";
+}
